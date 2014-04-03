d51e9a92bf5929b0145b920e03315037e01fc8be
Eric Britsman <eric_britsman@hotmail.com>
2014-04-03 14:04:47 +0200

    Account picker now functional, can either pick account (resulting in login) or delete stored account. Currently the back button on phone is used to navigate back to login screen. Highest prio now is to integrate with stuff that calls the actual github api (both to use accounts and to verify that accounts really exist).

--- app/src/main/java/com/EDA397/Navigator/Navigator/AccountPickerActivity.java
+++ app/src/main/java/com/EDA397/Navigator/Navigator/AccountPickerActivity.java
@@ -1,57 +1,51 @@
 package com.EDA397.Navigator.Navigator;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v4.widget.SimpleCursorAdapter;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 
-public class AccountPickerActivity extends ListActivity implements AdapterView.OnItemClickListener {
+public class AccountPickerActivity extends Activity implements AdapterView.OnItemClickListener {
 
     private SharedPreferences accounts;
     private SharedPreferences current;
     private SharedPreferences.Editor accEdit;
     private SharedPreferences.Editor currEdit;
-    Set<String> columns;
-    String[] sc;
-    ListView listView;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_account_picker);
+        setContentView(R.layout.activity_account_picker);
+        ArrayList<String> columns = new ArrayList<String>();
         accounts = getSharedPreferences("StoredAccounts", MODE_PRIVATE);
         current = getSharedPreferences("CurrentAccount", MODE_PRIVATE);
         accEdit = accounts.edit();
         currEdit = current.edit();
         // For the cursor adapter, specify which columns go into which views
-        columns = accounts.getAll().keySet();
-        sc = new String[columns.size()];
-        int i =0;
-        for (String s : columns){
-            sc[i] = s;
-            i++;
-        }
-        listView = getListView();
+        Set<String> c = accounts.getAll().keySet();
+        columns.addAll(c);
+        listView = (ListView) findViewById(R.id.account_list);
+        listView.setClickable(true);
+        listView.setOnItemClickListener(this);
 
         //setting adapter
-        listView.setAdapter(new ArrayAdapter<String>(getApplicationContext(),R.layout.account_item,
-                R.id.account_text, sc));
-        listView.setOnItemClickListener(this);
+        listView.setAdapter(new AccountAdapter(getApplicationContext(),R.layout.account_item,
+                R.id.account_text, columns));
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        String s = ((TextView)view.findViewById(R.id.account_text)).getText().toString();
+        final String s = ((TextView)view.findViewById(R.id.account_text)).getText().toString();
         //Check github login here as well?
         currEdit.clear();
         currEdit.putString("name", s);
@@ -69,5 +63,9 @@ public class AccountPickerActivity extends ListActivity implements AdapterView.O
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    public void deleteAcc(String s){
+        accEdit.remove(s);
+        accEdit.commit();
     }
 }
