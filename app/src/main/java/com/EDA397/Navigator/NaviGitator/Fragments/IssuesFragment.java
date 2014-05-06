0e76f10bf9f2927d02b9ff0592af696b1601bc64
linlinwang <bellis.lin@gmail.com>
2014-05-06 14:42:36 +0200

    List the issues in expandListview

--- app/src/main/java/com/EDA397/Navigator/NaviGitator/Fragments/IssuesFragment.java
+++ app/src/main/java/com/EDA397/Navigator/NaviGitator/Fragments/IssuesFragment.java
@@ -7,16 +7,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 
 import com.EDA397.Navigator.NaviGitator.Activities.GitFunctionality;
-import com.EDA397.Navigator.NaviGitator.Activities.RepositoryActivity;
+import com.EDA397.Navigator.NaviGitator.Adapters.ExpandableListAdapter;
 import com.EDA397.Navigator.NaviGitator.R;
 
 import org.eclipse.egit.github.core.Issue;
-import org.eclipse.egit.github.core.RepositoryCommit;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -28,25 +29,61 @@ public class IssuesFragment extends Fragment implements AdapterView.OnItemClickL
     private List<Issue> repoIssues;
     private View view;
 
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_issues, container, false);
-        git = GitFunctionality.getInstance();
+        // get the listview
+        expListView =(ExpandableListView) view.findViewById(R.id.lvExp);
+        // preparing list data
+        prepareListData();
+        listAdapter = new ExpandableListAdapter(getActivity(), listDataHeader, listDataChild);
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+
+       /* git = GitFunctionality.getInstance();
         repoIssues = git.getRepoIssues();
         ArrayList<String> issueMsg = new ArrayList<String>();
-
         for(Issue i : repoIssues) {
-            issueMsg.add("Title: " + i.getTitle() + " Body: " + i.getBody());
+           // issueMsg.add("Title: " + i.getTitle() + " Body: " + i.getBody());
+            issueMsg.add(i.getTitle());
         }
         listView = (ListView) view.findViewById(R.id.issues_list);
         listView.setClickable(true);
         listView.setOnItemClickListener(this);
         //Consider making a custom adapter for commits (if we want to extend the ListItems to hold multiple things).
         listView.setAdapter(new ArrayAdapter(view.getContext(), android.R.layout.simple_list_item_1, issueMsg.toArray()));
-
+*/
         return view;
+    }
+    private void prepareListData() {
+
+
+        listDataChild = new HashMap<String, List<String>>();
+
+        //list of issue
+        git = GitFunctionality.getInstance();
+        repoIssues = git.getRepoIssues();
+        listDataHeader = new ArrayList<String>();
+        for(Issue i : repoIssues) {
+            listDataHeader.add(i.getTitle());
+        }
+        //list comments of the issue
+        ArrayList<String> issueComment = new ArrayList<String>();
+         for(Issue i : repoIssues){
+          issueComment.add(i.getBody());
+        }
+        //for(Issue i:repoIssues) {
+          //  listDataChild.put(listDataHeader.get(i.getNumber()), issueComment);
+        //}
+
     }
 
     @Override
@@ -54,9 +91,10 @@ public class IssuesFragment extends Fragment implements AdapterView.OnItemClickL
      * Navigates to comments tab after a issue has been selected.
      */
     public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-        git = GitFunctionality.getInstance();
-        git.setCurrentIssue(repoIssues.get(position));
-        RepositoryActivity r = (RepositoryActivity) getActivity();
-        r.viewPager.setCurrentItem(3);
+       // git = GitFunctionality.getInstance();
+       // git.setCurrentIssue(repoIssues.get(position));
+       // RepositoryActivity r = (RepositoryActivity) getActivity();
+       // r.viewPager.setCurrentItem(3);
+
     }
 }
