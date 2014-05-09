2074a44cbba739ceb9ce792e641a5f3821363512
Anton Bretting <sajfer@gmail.com>
2014-05-09 14:42:23 +0200

    Added status to the PivotalStory

--- app/src/main/java/com/EDA397/Navigator/NaviGitator/Datatypes/PivotalStory.java
+++ app/src/main/java/com/EDA397/Navigator/NaviGitator/Datatypes/PivotalStory.java
@@ -4,16 +4,24 @@ package com.EDA397.Navigator.NaviGitator.Datatypes;
  * Created by sajfer on 2014-05-05.
  */
 public class PivotalStory {
+
+    public static enum StoryStatus
+    {
+        noStatus, unscheduled, unstarted, started, accepted, finished, delivered, rejected
+    }
+
     private String name;
     private Integer id;
     private String description;
     private Integer owner;
+    private StoryStatus status;
 
-    public PivotalStory(String name, Integer id, String description, Integer owner){
+    public PivotalStory(String name, Integer id, String description, Integer owner, StoryStatus status){
         this.name = name;
         this.id = id;
         this.description = description;
         this.owner = owner;
+        this.status = status;
     }
 
     public void setId(Integer id) {
@@ -46,5 +54,13 @@ public class PivotalStory {
 
     public Integer getOwner() {
         return owner;
+    }
+
+    public void setStatus(StoryStatus status) {
+        this.status = status;
+    }
+
+    public StoryStatus getStatus() {
+        return status;
     }
 }
