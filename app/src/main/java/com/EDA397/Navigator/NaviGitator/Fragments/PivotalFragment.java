52593db80f111e28852f9bbb53382f1de5dac2ad
Meiso <meiha91@gmail.com>
2014-05-12 14:47:36 +0200

    added expandablelistadapter that doesnt work yet

--- app/src/main/java/com/EDA397/Navigator/NaviGitator/Fragments/PivotalFragment.java
+++ app/src/main/java/com/EDA397/Navigator/NaviGitator/Fragments/PivotalFragment.java
@@ -8,6 +8,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,18 +17,22 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.EDA397.Navigator.NaviGitator.Datatypes.PivotalProject;
+import com.EDA397.Navigator.NaviGitator.Datatypes.PivotalStory;
 import com.EDA397.Navigator.NaviGitator.Services.NotificationService;
+import com.EDA397.Navigator.NaviGitator.Adapters.ExpandableListAdapter;
 import com.EDA397.Navigator.NaviGitator.R;
 import com.EDA397.Navigator.NaviGitator.SupportFunctions.PivotalFunctionality;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-public class PivotalFragment extends Fragment {
+public class PivotalFragment extends Fragment implements AdapterView.OnItemClickListener{
 
     private View view;
     private List<String> projNames;
     private PivotalFunctionality pivotal;
+    private HashMap<String,List<String>> storyNames;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -50,7 +55,7 @@ public class PivotalFragment extends Fragment {
 
                         final String name = ((EditText)view.findViewById(R.id.pivotalUsername)).getText().toString().trim().toLowerCase();
                         final String pw = ((EditText)view.findViewById(R.id.pivotalPassword)).getText().toString().trim();
-                        ListView lv = (ListView)view.findViewById(R.id.pivotalResult);
+                        ExpandableListView lv = (ExpandableListView)view.findViewById(R.id.expandableListView);
                         pivotal = PivotalFunctionality.getInstance();
 
                         if(name.length() < 1 || pw.length() < 1){
@@ -64,6 +69,7 @@ public class PivotalFragment extends Fragment {
 
                             lv.setAdapter(new ArrayAdapter<String>(view.getContext(),
                                     android.R.layout.simple_list_item_1, getPivoData()));
+
                         }
                         else{
                             Toast toast = Toast.makeText(getActivity().getApplicationContext(),
@@ -80,10 +86,23 @@ public class PivotalFragment extends Fragment {
     private List<String> getPivoData(){
         pivotal = PivotalFunctionality.getInstance();
         List<PivotalProject> projects = pivotal.getPivotalProjects();
+
         projNames = new ArrayList<String>();
         for(PivotalProject proj : projects){
+            List<PivotalStory> stories = pivotal.getPivotalStories(proj.getId());
             projNames.add(proj.getName());
+            List<String> story = new ArrayList<String>();
+            for(PivotalStory stor : stories){
+                story.add(stor.getName());
+            }
+            storyNames.put(proj.getName(),story);
         }
+
         return projNames;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
     }
 }
